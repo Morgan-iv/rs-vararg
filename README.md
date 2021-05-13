3afd6de0f59b2503cf5717d5b9d75bf21d438465
Alexey Ivushkin <ivushkinalex@gmail.com>
2021-05-13 04:09:18 +0300

    Add docs

--- README.md
+++ README.md
@@ -1,7 +1,88 @@
 # Vararg
 ![vararg tests status](https://github.com/Morgan-iv/rs-vararg/actions/workflows/rust.yml/badge.svg)
 
-This crate provides `vararg` proc-macro, that can be applied to any fn which last arg is `Vec`, array or slice, turning it into vararg macro like `println!` or `format!`
+This crate provides `vararg` proc-macro, that can be applied to any
+function which last arg is `Vec`, array or reference to slice, turning
+it into variadic macro, similar to `println!` or `format!`
+
+## Examples
+
+```rust
+use vararg::vararg;
+
+#[vararg]
+fn vararg_func<const L: usize>(a: &str, arr: [&str; L]) -> String {
+    format!("\nfirst: {} \nlast: {} \n", a, arr.join(","))
+}
+
+fn main() {
+    let s1 = "\n\
+        first: first \n\
+        last:  \n\
+    ";
+    let s2 = "\n\
+        first: first \n\
+        last: last \n\
+    ";
+    let s3 = "\n\
+        first: first \n\
+        last: last1,last2 \n\
+    ";
+    assert_eq!(s1, vararg_func!("first"));
+    assert_eq!(s2, vararg_func!("first", "last"));
+    assert_eq!(s3, vararg_func!("first", "last1", "last2"));
+}
+```
+
+You can create vararg function without first required argument (unlike
+vararg functions in C or nightly Rust). For example, let's create vararg
+function to join String slices without separator:
+
+```rust
+use vararg::vararg;
+
+#[vararg]
+fn join_strs<const L: usize>(arr: [&str; L]) -> String {
+    arr.join("")
+}
+
+fn main() {
+    let s1 = "";
+    let s2 = "ha";
+    let s3 = "hahaha";
+    assert_eq!(s1, join_strs!());
+    assert_eq!(s2, join_strs!("ha"));
+    assert_eq!(s3, join_strs!("ha", "ha", "ha"));
+}
+```
+
+You also can change name of generated macro or type of last arg
+
+```rust
+use vararg::vararg;
+use std::process::Command;
+
+#[vararg(name = exec, type = slice)] // or type = array (default), or type = vec
+fn exec_process(basename: &str, args: &[&str]) -> String {
+    String::from_utf8(
+        Command::new(basename)
+            .args(args)
+            .output()
+            .expect("failed to execute echo")
+            .stdout,
+    )
+    .expect("failed to parse hello")
+}
+
+fn main() {
+    let (name, c) = if cfg!(target_os = "windows") {
+        ("cmd", "/C")
+    } else {
+        ("sh", "-c")
+    };
+    assert_eq!("hello\n", exec!(name, c, "echo hello"));
+}
+```
 
 ## License
 Licensed under either of Apache License, Version 2.0 or MIT license at your option.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,86 @@
+//! This crate provides [`macro@vararg`] proc-macro, that can be applied to any
+//! function which last arg is [`Vec`], [array] or reference to [slice], turning
+//! it into variadic macro, similar to `println!` or `format!`
+//!
+//! # Example
+//!
+//! ```
+//! # use vararg::vararg;
+//! #
+//! #[vararg]
+//! fn vararg_func<const L: usize>(a: &str, arr: [&str; L]) -> String {
+//!     format!("\nfirst: {} \nlast: {} \n", a, arr.join(","))
+//! }
+//!
+//! fn main() {
+//!     let s1 = "\n\
+//!         first: first \n\
+//!         last:  \n\
+//!     ";
+//!     let s2 = "\n\
+//!         first: first \n\
+//!         last: last \n\
+//!     ";
+//!     let s3 = "\n\
+//!         first: first \n\
+//!         last: last1,last2 \n\
+//!     ";
+//!     assert_eq!(s1, vararg_func!("first"));
+//!     assert_eq!(s2, vararg_func!("first", "last"));
+//!     assert_eq!(s3, vararg_func!("first", "last1", "last2"));
+//! }
+//! ```
+//!
+//! You can create vararg function without first required argument (unlike
+//! vararg functions in C or nightly Rust). For example, let's create vararg
+//! function to join String slices without separator:
+//!
+//! ```
+//! # use vararg::vararg;
+//! #
+//! #[vararg]
+//! fn join_strs<const L: usize>(arr: [&str; L]) -> String {
+//!     arr.join("")
+//! }
+//!
+//! fn main() {
+//!     let s1 = "";
+//!     let s2 = "ha";
+//!     let s3 = "hahaha";
+//!     assert_eq!(s1, join_strs!());
+//!     assert_eq!(s2, join_strs!("ha"));
+//!     assert_eq!(s3, join_strs!("ha", "ha", "ha"));
+//! }
+//! ```
+//!
+//! You also can change name of generated macro or type of last arg
+//!
+//! ```
+//! # use vararg::vararg;
+//! use std::process::Command;
+//!
+//! #[vararg(name = exec, type = slice)] // or type = array (default), or type = vec
+//! fn exec_process(basename: &str, args: &[&str]) -> String {
+//!     String::from_utf8(
+//!         Command::new(basename)
+//!             .args(args)
+//!             .output()
+//!             .expect("failed to execute echo")
+//!             .stdout,
+//!     )
+//!     .expect("failed to parse hello")
+//! }
+//!
+//! fn main() {
+//!     let (name, c) = if cfg!(target_os = "windows") {
+//!         ("cmd", "/C")
+//!     } else {
+//!         ("sh", "-c")
+//!     };
+//!     assert_eq!("hello\n", exec!(name, c, "echo hello"));
+//! }
+//! ```
+
 use proc_macro::{TokenStream, TokenTree};
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
@@ -13,6 +96,26 @@ struct VarargConfig {
     prefix: TokenStream2,
 }
 
+/// The whole point.
+///
+/// Creates variadic macro from function
+///
+/// # Params
+///
+/// This proc-macro has 2 optional params:
+/// - `name` - specify name of the resulting macro (default: fn's name).
+///     Name can be any suitable identifier
+/// - `type` - specify type of the last argument in function. Possible types:
+///     - `array` (this is the default type)
+///     - `vec`
+///     - `slice`
+///
+/// Params can be set like `#[vararg(name = name1, type = array)]`,
+/// or with trailing comma: `#[vararg(name = name2, type = vec,)]`
+///
+/// Currently, you can change the value of the previously set param by setting
+/// it again like this: `#[vararg(name = name1, type = array, name = name2)]`,
+/// but this behaviour may (and probably will) change in future
 #[proc_macro_attribute]
 pub fn vararg(attrs: TokenStream, input: TokenStream) -> TokenStream {
     let func = parse_macro_input!(input as ItemFn);
